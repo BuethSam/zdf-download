88a63ae7e4699d975912623d09b842da8878de85
Sam Büth <Samuel1bueth@gmail.com>
2024-01-08 11:06:24 +0000

    feat: set debug log level with env var

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 """Main module for the ZDF Downloader."""
-import time
-import schedule
 import logging
-from zdf_download import ZDFDownload
+import os
+import time
+
+import schedule
+
 from configuration import Configuration, load_configuration_from_yaml
 from history import History
+from zdf_download import ZDFDownload
+
 
 def main() -> None:
+
 
     config: Configuration = load_configuration_from_yaml("configuration/configuration.yaml")
     history: History = History("configuration/history.yaml")
     zdf_downloader: ZDFDownload = ZDFDownload(history=history, config=config)
 
     log = logging.getLogger("zdf-download")
+    if os.environ.get("DEBUG", False) == "True":
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
     log.info("launching application")
 
     schedule.every(config.interval).minutes.do(zdf_downloader.check_all_shows, shows=config.shows)
